d077cb15babfdcea316abfc7d56884d824f78e63
alasgarragimov <alasgar.ragimov01@gmail.com>
2023-10-03 15:10:54 +0400

    i added console_log

--- index.js
+++ index.js
@@ -7,45 +7,49 @@ var points = 0;
 
 if (start) {
 
-    var q1 = prompt("Goy uzu ne rengdedir ?")
+    var q1 = prompt("Goy uzu ne rengdedir ?");
 
     if (q1 === "Mavi" || q1 === "Goy" ||
         q1 === "mavi" || q1 === "goy") {
 
         points += 10;
-        alert(`Tebrikler ${points} xal qazandiniz`)
+        alert(`Tebrikler ${points} xal qazandiniz`);
+        console.log(`Tebrikler birinci musahibeden kecdiniz`);
 
-        var q2 = prompt("Serxan mel nece yasi var?")
+        var q2 = prompt("Serxan mel nece yasi var?");
 
         if (q2 == 29) {
             points += 10;
-            alert(
-                `Tebrikler Sizin umumi xalinizin cemi ${points} oldu`)
+            alert(`Tebrikler Sizin umumi xalinizin cemi ${points} oldu`);
+            console.log(`Tebrikler ikinci musahibeden kecdiniz`);
 
-            var q3 = prompt("Serxan mel hansi burcdur ? )))")
+            var q3 = prompt("Serxan mel hansi burcdur ? )))");
 
             if (q3 === "Dolca" || q3 === "dolca") {
                 points += 10;
-                alert(`Tebrikler Sizin umumi xalinizin cemi ${points} oldu`)
+                alert(`Tebrikler Sizin umumi xalinizin cemi ${points} oldu`);
+                console.log(`Tebrikler ucuncu musahibeden kecdiniz`);
 
             } else {
                 points -= 10;
-                alert(`Teesufki sehf cavab verdiyiniz ucun siz 10 xal itirdiniz. Xaliniz ${points} `)
-
-
+                alert(`Teesufki sehf cavab verdiyiniz ucun siz 10 xal itirdiniz. Xaliniz ${points} `);
+                console.log(`Teesufki sehf cavab verdiyiniz ucun siz 10 xal itirdiniz.`);
             }
 
         } else {
             points -= 10;
-            alert(`Teesufki sehf cavab verdiyiniz ucun siz 10 xal itirdiniz. Xalinizin ${points}`)
+            alert(`Teesufki sehf cavab verdiyiniz ucun siz 10 xal itirdiniz. Xalinizin ${points}`);
+            console.log(`Teesufki sehf cavab verdiyiniz ucun siz 10 xal itirdiniz.`);
         }
 
     } else {
         points = 0;
-        alert(`Teessufki siz sehv cavab verdiniz. Xaliniz ${points}`)
+        alert(`Teessufki siz sehv cavab verdiniz.Xaliniz ${points}`);
+        console.log(`Teessufki siz sehv cavab verdiniz.`);
     }
 
 
 } else {
     alert("Gozumuz aydin !")
+    console.log("Gozumuz aydin !");
 }
